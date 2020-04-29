964ba7ac2bc05f755133eb3820bf7a6380601382
Jessica Fernando <jessloufern@gmail.com>
2020-04-28 23:49:14 -0700

    more icon set

--- conf/default.php
+++ conf/default.php
@@ -8,4 +8,5 @@ $conf['discussionPage']   = 'discussion:@ID@';
 $conf['userPage']         = 'user:@USER@:';
 $conf['hideTools']        = 0;
 $conf['showIcon']         = 1;
-
+$conf['icon']             = 3;
+$conf['favicon']          = 3;
