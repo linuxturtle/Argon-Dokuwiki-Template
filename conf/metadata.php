bb4c567ed43e0b3bf008b445b92140aee337632f
Jessica Fernando <jessloufern@gmail.com>
2020-04-28 23:48:27 -0700

    Icon set, icon config, and screenshots

--- conf/metadata.php
+++ conf/metadata.php
@@ -8,3 +8,5 @@ $meta['discussionPage']   = array('string');
 $meta['userPage']         = array('string');
 $meta['hideTools']        = array('onoff');
 $meta['showIcon']         = array('onoff');
+$meta['icon']             = array(1,2,3,4);
+$meta['favicon']          = array(1,2,3,4);
